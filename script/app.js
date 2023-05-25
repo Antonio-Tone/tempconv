8c7a1149ffe2b2d9e9577390d10625dfba550222
Antonio-Tone <Antoniotone815@gmail.com>
2023-05-25 22:06:18 +0200

    first commit

---
+++ script/app.js
@@ -0,0 +1,6 @@
+let celsiusInput = document.getElementById('cel');
+let fahrenheitInput = document.getElementById('fah');
+let kelvinInput = document.getElementById('kel');
+let btn = document.getElementById('btn');
+
+ 
